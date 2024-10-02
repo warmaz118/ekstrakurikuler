3bd7df78defc0ed192cf99a7671c9b1c2614eb01
warmaz118 <mikhsaan118@gmail.com>
2024-10-02 21:30:54 +0700

    menambahkan halaman views

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -30,7 +30,7 @@
     <li class="mb-1 group">
         <a href="" class="flex font-semibold items-center py-2 px-4 text-gray-900 hover:bg-gray-950 hover:text-gray-100 rounded-md group-[.active]:bg-gray-800 group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100 sidebar-dropdown-toggle">
             <i class='bx bxl-blogger mr-3 text-lg' ></i>                 
-            <span class="text-sm">Post</span>
+            <span class="text-sm">Ekstrakurikuler</span>
             <i class="ri-arrow-right-s-line ml-auto group-[.selected]:rotate-90"></i>
         </a>
         <ul class="pl-7 mt-2 hidden group-[.selected]:block">
